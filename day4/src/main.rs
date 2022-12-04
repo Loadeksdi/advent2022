07df9c587523bbb97e7038b362443fd471b83d2f
Loadeksdi <nicolas.load@gmail.com>
2022-12-04 12:20:59 +0100

    Day 1 part 1

---
+++ day4/src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
+
+fn main() {
+    let mut overlapping_areas_count = 0;
+    if let Ok(lines) = read_lines("input.txt") {
+        for line in lines {
+            if let Ok(area) = line {
+                let vec: Vec<&str> = area.split(",").collect();
+                let zone_one: Vec<&str> = vec[0].split("-").collect();
+                let zone_two: Vec<&str> = vec[1].split("-").collect();
+                let zone_one_begin: i32 = zone_one[0].parse().unwrap();
+                let zone_one_end: i32 = zone_one[1].parse().unwrap();
+                let zone_two_begin: i32 = zone_two[0].parse().unwrap();
+                let zone_two_end: i32 = zone_two[1].parse().unwrap();
+                if (zone_one_begin >= zone_two_begin && zone_one_end <= zone_two_end)
+                    || (zone_two_begin >= zone_one_begin && zone_two_end <= zone_one_end)
+                {
+                    overlapping_areas_count += 1;
+                }
+            }
+        }
+    }
+    println!("Number of overlapping areas: {}", overlapping_areas_count);
+}
+
+// Read input file, taken from rust-by-example
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
