9eb0b56332155752542a32f8292ec3cb16f94710
Loadeksdi <nicolas.load@gmail.com>
2022-12-05 15:27:05 +0100

    Day5 part1 done

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -22,51 +22,47 @@ fn main() {
                 if crate_line.starts_with(" 1") || crate_line == "" {
                     continue;
                 } else if crate_line.starts_with("move") {
-                    let instructions: Vec<usize> = crate_line
-                        .chars()
-                        .filter(|c| c.is_digit(10))
-                        .map(|c| c.to_digit(10).unwrap() as usize)
-                        .collect();
-                    let number_of_crate_to_move = instructions[0];
-                    let from_stack = instructions[1];
-                    let to_stack = instructions[2];
+                    let instructions = crate_line
+                        .split_whitespace()
+                        .filter_map(|s| s.parse().ok())
+                        .collect::<Vec<usize>>();
+                    let number_of_crate_to_move: usize = instructions[0];
+                    let from_stack: usize = instructions[1];
+                    let to_stack: usize = instructions[2];
                     for _i in 0..(number_of_crate_to_move) {
                         let crate_in_stack = stacks[from_stack - 1].pop();
-                        println!(
-                            "Moving crate {:?} from stack {:?} to stack {:?}",
-                            crate_in_stack,
-                            from_stack,
-                            to_stack
-                        );
                         if crate_in_stack.is_none() {
                             continue;
-                        }                        
+                        }
                         stacks[to_stack - 1].push(crate_in_stack.unwrap());
                     }
                 } else {
                     let vec: Vec<&str> = crate_line.split(" ").collect();
                     let mut counter = 0;
-                    let mut empty = false;
+                    let mut four = 0;
                     for ele in vec {
                         if ele != "" {
+                            four = 0;
                             let stack = &mut stacks[counter];
                             stack.insert(0, ele.chars().nth(1).unwrap());
                             stacks[counter] = stack.to_vec();
                             counter += 1;
                         } else {
-                            if !empty {
+                            four += 1;
+                            if four % 4 == 0 {
                                 counter += 1;
                             }
-                            empty = true;
                         }
                     }
                 }
             }
         }
     }
+    let mut code: String = "".to_string();
     for i in 0..stacks.len() {
-        println!("Stack {}: {:?}", i, stacks.get(i).unwrap().last());
+        code.push(*stacks.get(i).unwrap().last().unwrap());
     }
+    println!("{}", code);
 }
 
 // Read input file, taken from rust-by-example
